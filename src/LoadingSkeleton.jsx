7dfc6a56b1a68e85d792ea99080dacc430464480
Johan Munneke <johanmunneke94@gmail.com>
2022-02-11 11:28:56 +0100

    Setup widget

--- src/LoadingSkeleton.jsx
+++ src/LoadingSkeleton.jsx
@@ -1,10 +1,54 @@
 import { Component, createElement } from "react";
 
-import { HelloWorldSample } from "./components/HelloWorldSample";
 import "./ui/LoadingSkeleton.css";
 
 export class LoadingSkeleton extends Component {
+    constructor(props) {
+        super(props);
+        this.isInitialized = false;
+    }
+
+    componentDidMount() {
+        // Once widget is mounted, set isInitialized to true, such that the content can be loaded
+        this.isInitialized = true;
+    }
+
+    /**
+     * Render the shapes and set the correct properties
+     *
+     * @returns list of skeleton shapes (divs)
+     */
+    renderShapes() {
+        let key = 0;
+        return this.props.skeletonShapes.map(shape => {
+            key++;
+            let className = shape.shapeClass + " skeletonShape ";
+            if (shape.skeletonShape === "rectangle") {
+                className += "skeletonRectangle";
+            } else {
+                className += "skeletonCircle";
+            }
+            const width = shape.shapeWidth + (shape.shapeWidthPixels ? "px" : "%");
+            const height = shape.shapeHeight + (shape.shapeHeightPixels ? "px" : "%");
+            return <div key={key} className={className} style={{ width: width, height: height }}></div>;
+        });
+    }
+
     render() {
-        return <HelloWorldSample sampleText={this.props.sampleText} />;
+        const dataLoaded = this.props.dataLoaded && this.props.dataLoaded.value;
+        // Once the widget is mounted, show the content, such that flows are triggered
+        let contentToShow;
+        if (this.isInitialized) {
+            // If date is not yet loaded, set class such that it is not shown
+            const classNameContent = dataLoaded ? "" : "skeletonContentNotVisible";
+            contentToShow = <div className={classNameContent}>{this.props.contentToLoad}</div>;
+        }
+
+        return (
+            <div className={this.props.class}>
+                {contentToShow}
+                {dataLoaded ? undefined : this.renderShapes()}
+            </div>
+        );
     }
 }
