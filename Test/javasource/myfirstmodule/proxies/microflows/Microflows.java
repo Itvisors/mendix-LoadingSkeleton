d5414c75aff05fd2feb7473efecc653f86c5b350
Johan Munneke <johanmunneke94@gmail.com>
2024-08-09 15:55:44 +0200

    Upgraded project to mendix 10

--- Test/javasource/myfirstmodule/proxies/microflows/Microflows.java
+++ Test/javasource/myfirstmodule/proxies/microflows/Microflows.java
@@ -4,32 +4,38 @@
 
 package myfirstmodule.proxies.microflows;
 
-import java.util.HashMap;
-import java.util.Map;
 import com.mendix.core.Core;
 import com.mendix.systemwideinterfaces.core.IContext;
 import com.mendix.systemwideinterfaces.core.IMendixObject;
 
-public class Microflows
+public final class Microflows
 {
 	/**
-	 * @deprecated
-	 * The default constructor of the Microflows class should not be used.
-	 * Use the static microflow invocation methods instead.
+	 * Private constructor to prevent instantiation of this class. 
 	 */
-	@java.lang.Deprecated(since = "9.12", forRemoval = true)
-	public Microflows() {}
+	private Microflows() {}
 
 	// These are the microflows for the MyFirstModule module
+	public static com.mendix.core.actionmanagement.MicroflowCallBuilder aSU_CreateDataBuilder()
+	{
+		com.mendix.core.actionmanagement.MicroflowCallBuilder builder = Core.microflowCall("MyFirstModule.ASU_CreateData");
+		return builder;
+	}
+
 	public static boolean aSU_CreateData(IContext context)
 	{
-		Map<java.lang.String, Object> params = new HashMap<>();
-		return (java.lang.Boolean) Core.microflowCall("MyFirstModule.ASU_CreateData").withParams(params).execute(context);
+		Object result = aSU_CreateDataBuilder().execute(context);
+		return (boolean) result;
 	}
+	public static com.mendix.core.actionmanagement.MicroflowCallBuilder dS_SkeletonHelper_CreateBuilder()
+	{
+		com.mendix.core.actionmanagement.MicroflowCallBuilder builder = Core.microflowCall("MyFirstModule.DS_SkeletonHelper_Create");
+		return builder;
+	}
+
 	public static myfirstmodule.proxies.SkeletonHelper dS_SkeletonHelper_Create(IContext context)
 	{
-		Map<java.lang.String, Object> params = new HashMap<>();
-		IMendixObject result = (IMendixObject)Core.microflowCall("MyFirstModule.DS_SkeletonHelper_Create").withParams(params).execute(context);
-		return result == null ? null : myfirstmodule.proxies.SkeletonHelper.initialize(context, result);
+		Object result = dS_SkeletonHelper_CreateBuilder().execute(context);
+		return result == null ? null : myfirstmodule.proxies.SkeletonHelper.initialize(context, (IMendixObject) result);
 	}
 }
